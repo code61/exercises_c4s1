1373de238cc71d042f4b7519d1e39224ddc1eead
Tom Close <tom.close@cantab.net>
2014-02-18 18:25:45 +0000

    Fix test

--- spec/penultimate_digit_spec.rb
+++ spec/penultimate_digit_spec.rb
@@ -3,11 +3,11 @@ require './dates'
 describe 'penultimate_digit function' do
 
   it "extracts the penultimate digit" do
-    expect(last_digit(1)).to   eq(0)
-    expect(last_digit(4)).to   eq(0)
-    expect(last_digit(17)).to  eq(1)
-    expect(last_digit(139)).to eq(3)
-    expect(last_digit(0)).to   eq(0)
+    expect(penultimate_digit(1)).to   eq(0)
+    expect(penultimate_digit(4)).to   eq(0)
+    expect(penultimate_digit(17)).to  eq(1)
+    expect(penultimate_digit(139)).to eq(3)
+    expect(penultimate_digit(0)).to   eq(0)
   end
 
 end
